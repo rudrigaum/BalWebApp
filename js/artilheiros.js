96fc7b7ffd3cd5fe71147ce1ddc7bb27f2721f96
Rodrigo Cerqueira <rodrigocerqueirareis@gmail.com>
2025-04-14 19:59:58 -0300

    add logos on top scorer app

--- js/artilheiros.js
+++ js/artilheiros.js
@@ -1,19 +1,35 @@
+import { escudos } from './dados.js';
+
 export function preencherArtilheiros(jogos) {
-    const contagem = {};
-  
-    jogos.forEach(({ marcadoresCasa, marcadoresFora }) => {
-      [...marcadoresCasa, ...marcadoresFora].forEach(jogador => {
-        contagem[jogador] = (contagem[jogador] || 0) + 1;
-      });
+  const contagem = {};
+  const timesPorJogador = {};
+
+  jogos.forEach(({ marcadoresCasa, marcadoresFora, casa, fora }) => {
+    marcadoresCasa.forEach(jogador => {
+      contagem[jogador] = (contagem[jogador] || 0) + 1;
+      timesPorJogador[jogador] = casa;
     });
-  
-    const lista = Object.entries(contagem)
-      .sort((a, b) => b[1] - a[1])
-      .map(([jogador, gols]) => `<li class="py-1">${jogador} - <strong>${gols}</strong> gol(s)</li>`)
-      .join("");
-  
-    document.getElementById("artilheiros").innerHTML = `
-      <ul class="text-sm">${lista || "<li>Nenhum gol registrado.</li>"}</ul>
-    `;
-  }
-  
+    marcadoresFora.forEach(jogador => {
+      contagem[jogador] = (contagem[jogador] || 0) + 1;
+      timesPorJogador[jogador] = fora;
+    });
+  });
+
+  const lista = Object.entries(contagem)
+    .sort((a, b) => b[1] - a[1])
+    .map(([jogador, gols]) => {
+      const time = timesPorJogador[jogador];
+      const escudo = escudos[time] || "";
+      return `
+        <li class="flex items-center gap-2 py-1">
+          ${escudo ? `<img src="${escudo}" alt="${time}" class="w-5 h-5" />` : ""}
+          <span>${jogador} - <strong>${gols}</strong> gol(s)</span>
+        </li>
+      `;
+    })
+    .join("");
+
+  document.getElementById("artilheiros").innerHTML = `
+    <ul class="text-sm">${lista || "<li>Nenhum gol registrado.</li>"}</ul>
+  `;
+}
